b3bfc71b6762255872d953592465f4629762ead8
unknown <rellowy@gmail.com>
2022-02-15 23:17:04 +0200

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'steam-ecd27',
+    appId: '1:476319000847:web:d05505f00693814f95767a',
+    storageBucket: 'steam-ecd27.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBPOz1WJ-Huy9ciPLIL5xC63Y21_gJMC7Y',
+    authDomain: 'steam-ecd27.firebaseapp.com',
+    messagingSenderId: '476319000847',
+  },
   production: false
 };
 
